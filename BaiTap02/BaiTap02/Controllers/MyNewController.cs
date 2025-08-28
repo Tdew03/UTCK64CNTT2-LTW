88943133b29445633ac7f21ee20387524c3c1c8b
Tdew03 <toan008003@gmail.com>
2025-08-28 23:52:21 +0700

    Bài tập

---
+++ BaiTap02/BaiTap02/Controllers/MyNewController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaiTap02.Controllers
+{
+    public class MyNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sample()
+        {
+            return View();
+        }
+    }
+}

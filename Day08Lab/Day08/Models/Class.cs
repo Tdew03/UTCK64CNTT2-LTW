ae0ad56eba6ec7956fa20a0f3955a75257b2ceaf
Tdew03 <toan008003@gmail.com>
2025-10-12 20:23:55 +0700

    add project

---
+++ Day08Lab/Day08/Models/Class.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Day08.Models
+{
+    public class Class
+    {
+        [Key]
+        public int ClassId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string ClassName { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}

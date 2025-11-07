7915927cf8358387949a633ac7cfa9763da2ae4f
Tdew03 <toan008003@gmail.com>
2025-11-07 22:55:28 +0700

    Bài thực hành 1

---
+++ BaiTH1/Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using BaiTH1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BaiTH1.Controllers
+{
+    [Route("Admin/Student")]// Tiền tố chung
+    public class StudentController : Controller
+    {
+        
+        private List<Student> listStudents = new List<Student>();
+        public StudentController()
+        {
+            listStudents = new List<Student>()
+            {
+                new Student()
+                {Id=101,Name = "Hải Nam ", Branch = Branch.IT, Gender = Gender.Male, IsRegular=true, Address = "A1-2018", Email="nam@g.com" },
+                new Student()
+                {Id=102,Name = "Minh Anh", Branch = Branch.BE, Gender = Gender.Female, IsRegular=false, Address = "B2-2019", Email="anh@g.com" },
+                new Student()
+                {Id=103,Name = "Hoàng Sơn", Branch = Branch.CE, Gender = Gender.Male, IsRegular=true, Address = "C3-2020", Email="son@g.com" },
+                new Student()
+                { Id=104,Name = "Phương Linh", Branch = Branch.EE, Gender = Gender.Female, IsRegular=false, Address = "D4-2021", Email="linh@g.com" }
+
+            };
+        }
+
+        [Route("List", Name = " StudentList")]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View(listStudents);
+        }
+
+        [HttpGet]
+        [Route("Add", Name = " StudentAdd")]
+        public IActionResult Create()
+        {
+            //Lấy danh sách các giá trị Gender để hiện thị radio button trên form
+            ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+            //Lấy danh sách các giá trị Branch để hiện thị select-option trên form
+            //Để hiện thị select-option trên View cần dùng List<SelectListItem>
+            ViewBag.AllBranches = new List<SelectListItem>()
+            {
+                new SelectListItem(){ Text="IT", Value = "1"},
+                new SelectListItem(){ Text="BE", Value = "2"},
+                new SelectListItem(){ Text="CE", Value = "3"},
+                new SelectListItem(){ Text="EE", Value = "4"},
+            };
+            return View();
+        }
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Create(Student s)
+        {
+            s.Id = listStudents.Last<Student>().Id + 1;
+            listStudents.Add(s);
+            return View("Index", listStudents);
+        }
+    }
+}
---
+++ BaiTH1/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace BaiTH1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }// Mã sinh viên
+        public string? Name { get; set; }// Tên sinh viên
+        public string? Email { get; set; }// Email sinh viên
+        public string? Password { get; set; }// Mật khẩu 
+        public Gender? Gender { get; set; }// Giới tính
+        public Branch? Branch { get; set; }// Ngành học
+        public bool IsRegular   { get; set; } //Hệ : true-chính quy, false- phi cq 
+        public string? Address { get; set; }// Địa chỉ
+        public DateTime DateOfBorth { get; set; }// Ngày sinh
+
+
+    }
+}

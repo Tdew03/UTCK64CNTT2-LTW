1446935b0c9a8703163dc7856fca413a2ed1556f
Tdew03 <toan008003@gmail.com>
2025-11-14 23:56:47 +0700

    Lab3

---
+++ Day13Lab/ViewComponents/RenderViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using BaiTH1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BaiTH1.ViewComponents
+{
+    public class RenderViewComponent : ViewComponent  
+    {
+        private List<MenuItem> menu;
+
+        public RenderViewComponent()
+        {
+            menu = new List<MenuItem>()
+            {
+                new MenuItem(){Id=1, Name="Branches", Link="/Branches/Index"},
+                new MenuItem(){Id=2, Name="Students", Link="/Student/Index"},
+                new MenuItem(){Id=3, Name="Subjects", Link="/Subjects/Index"},
+                new MenuItem(){Id=4, Name="Courses", Link="/Courses/Index"}
+            };
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("RenderLeftMenu", menu);
+        }
+    }
+}
